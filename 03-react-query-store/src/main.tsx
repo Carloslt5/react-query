d33b4def5d786026d371d8a08d7ce17b538e7fca
Carloslt <carloslt.proyectos@gmail.com>
2024-03-20 21:04:55 +0100

    ✅ feature: install tanstack query

--- 03-react-query-store/src/main.tsx
+++ 03-react-query-store/src/main.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router.tsx';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router.tsx";
 
-import './index.css';
+import "./index.css";
+import { AppProvider } from "./providers/app-provider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <main className="dark text-foreground bg-background">
-        <RouterProvider router={ router } />
-      </main>
-    </NextUIProvider>
-  </React.StrictMode>,
-)
+    <AppProvider>
+      <NextUIProvider>
+        <main className="dark text-foreground bg-background">
+          <RouterProvider router={router} />
+        </main>
+      </NextUIProvider>
+    </AppProvider>
+  </React.StrictMode>
+);
